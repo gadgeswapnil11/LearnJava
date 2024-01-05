fc41c02f80dd8937f3e9b8afd9adb67943ea4ee1
gadgeswapnil11 <gadgeswapnil11@gmail.com>
2024-01-05 19:16:44 +0530

    Frame work

---
+++ src/Java8Features/Lambda.java
@@ -0,0 +1,10 @@
+package Java8Features;
+
+public class Lambda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Java8Features/StramAPI.java
@@ -0,0 +1,10 @@
+package Java8Features;
+
+public class StramAPI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
